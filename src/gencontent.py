7259063632f699e3e9f4c5d2bd23cf5f7a92645a
Aki-cmd <valentin.gill@ges.thm.de>
2025-07-16 16:49:12 +0200

    set up github pages

--- src/gencontent.py
+++ src/gencontent.py
@@ -12,38 +12,41 @@ def extract_title(markdown):
         raise Exception("No H1 Header found")
     
 
-def generate_page(from_path, template_path, dest_path):
-    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
-    
-    with open(from_path, "r", encoding="utf-8") as f:
-        md_file = f.read()  
-        
-    with open(template_path, "r", encoding="utf-8") as f:           
-        template_file = f.read()
-    
-    headline = extract_title(md_file)
-    
-    html = markdown_to_html_node(md_file)
-    
+def generate_page(from_path, template_path, dest_path, basepath):
+    print(f" * {from_path} {template_path} -> {dest_path}")
+    from_file = open(from_path, "r")
+    markdown_content = from_file.read()
+    from_file.close()
 
-    html_str = html.to_html()
-    new = template_file.replace("{{ Title }}", headline)
-    final = new.replace("{{ Content }}", html_str)
+    template_file = open(template_path, "r")
+    template = template_file.read()
+    template_file.close()
+
+    node = markdown_to_html_node(markdown_content)
+    html = node.to_html()
+
+    title = extract_title(markdown_content)
+    template = template.replace("{{ Title }}", title)
+    template = template.replace("{{ Content }}", html)
+    template = template.replace('href="/','href="{basepath}')
+    template = template.replace('src="/','src="{basepath}')
+
+    dest_dir_path = os.path.dirname(dest_path)
+    if dest_dir_path != "":
+        os.makedirs(dest_dir_path, exist_ok=True)
+    to_file = open(dest_path, "w")
+    to_file.write(template)
+
     
-    os.makedirs(os.path.dirname(dest_path), exist_ok=True)
-    with open(dest_path, "w", encoding="utf-8") as f:
-        f.write(final)
-        
-    
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for filename in os.listdir(dir_path_content):
         from_path = os.path.join(dir_path_content, filename)
         dest_path = os.path.join(dest_dir_path, filename)
         if os.path.isfile(from_path):
             dest_path = Path(dest_path).with_suffix(".html")
-            generate_page(from_path, template_path, dest_path)
+            generate_page(from_path, template_path, dest_path, basepath)
         else:
-            generate_pages_recursive(from_path, template_path, dest_path)
+            generate_pages_recursive(from_path, template_path, dest_path, basepath)
 
             
 
--- src/main.py
+++ src/main.py
@@ -1,26 +1,32 @@
 import os
 import shutil
-
+import sys
 from copystatic import copy_files_recursive
 from gencontent import generate_pages_recursive
 
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+#dir_path_public = "./public"
+dir_path_docs = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
 
+basepath = "/"
+if sys.argv[0]:
+    basepath = sys.argv[0]
 
 def main():
+    
+    
     print("Deleting public directory...")
-    if os.path.exists(dir_path_public):
-        shutil.rmtree(dir_path_public)
+    if os.path.exists(dir_path_docs):
+        shutil.rmtree(dir_path_docs)
 
     print("Copying static files to public directory...")
-    copy_files_recursive(dir_path_static, dir_path_public)
+    copy_files_recursive(dir_path_static, dir_path_docs)
 
     print("Generating content...")
-    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
+    generate_pages_recursive(dir_path_content, template_path, dir_path_docs, basepath)
 
 
 main()
